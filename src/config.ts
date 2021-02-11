b1cd6557eca2e31afd9c584cb4812dbcda9b2bd7
tgouty <goutybattlenet@gmail.com>
2021-02-11 16:05:22 -0500

    demo DONE

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,17 @@
+import * as dotenv from "dotenv"
+
+export enum environments {
+    PRODUCTION = "production",
+    STAGING = "staging",
+    DEVELOPMENT = "development",
+    LOCAL = "local"
+}
+
 export default class Config {
     private static instance: Config
     public environment: string
     public port: number
-    public prettyLog: boolean
     public apiKey: string
-    public jwtSecretKey: string
-    public jwtTtl: string
 
     public static getInstance = (): Config => {
         if (!Config.instance) {
@@ -15,9 +21,28 @@ export default class Config {
     }
 
     private constructor() {
+        let path = ""
+
+        switch (process.env.NODE_ENV) {
+            case environments.PRODUCTION:
+                path = `${__dirname}/../_production.env`
+                break
+            case environments.STAGING:
+                path = `${__dirname}/../_staging.env`
+                break
+            case environments.DEVELOPMENT:
+                path = `${__dirname}/../_development.env`
+                break
+            default:
+                path = `${__dirname}/../_local.env`
+        }
+
+        dotenv.config({
+            path: path
+        })
+
+        this.environment = process.env.NODE_ENV || environments.LOCAL
         this.port = parseInt(process.env.NODE_PORT) || parseInt(process.env.PORT)
         this.apiKey = process.env.API_KEY
-        this.jwtSecretKey = process.env.JWT_SECRET_KEY
-        this.jwtTtl = process.env.JWT_TTL
     }
 }
